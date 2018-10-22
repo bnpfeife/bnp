9b2439ea3f1a142b12367a158d93f49ca3246a0a
Brandon Pfeifer <bnpfeife@gmail.com>
2018-10-21 22:05:50 -0400

    Remove comments.

--- bnpc_stack.h
+++ bnpc_stack.h
@@ -6,22 +6,18 @@
 #define bnpc_stack bnpc_vector
 
 BNP_FORCE_INLINE void bnpc_stack_init(struct bnpc_stack* stack, bnp_size element_size, bnp_size reserved) {
-  // hopefully this flattens out
   bnpc_vector_init(stack, element_size, reserved);
 }
 
 BNP_FORCE_INLINE void bnpc_stack_free(struct bnpc_stack* stack) {
-  // hopefully this flattens out
   bnpc_vector_free(stack);
 }
 
 BNP_FORCE_INLINE void bnpc_stack_push(struct bnpc_stack* stack, void* element) {
-  // hopefully this flattens out
   bnpc_vector_push(stack, element);
 }
 
 BNP_FORCE_INLINE void bnpc_stack_pop(struct bnpc_stack* stack, void* element) {
-  // hopefully this flattens out
   bnpc_vector_pop(stack, element);
 }
 
